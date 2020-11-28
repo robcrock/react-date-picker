f557e465d28ba8f8b4e6f8cb1b7dce570da6a8d4
robcrockPI <robert@propellerindustries.com>
2020-11-28 17:04:45 -0300

    Figured out how to grab the parameter values from the workbook.

--- src/components/Viz.js
+++ src/components/Viz.js
@@ -1,23 +1,53 @@
-import { useRef, useEffect } from "react"
+import { useState, useRef, useEffect } from "react"
 
 const { tableau } = window
 
 export default function Viz() {
+  const [viz, setViz] = useState(null)
+  // Placeholder div
   const ref = useRef(null)
+  const url =
+    "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DatePicker/Dashboard"
+  const options = {
+    hideTabs: true,
+    hideToolbar: true,
+    width: "900px",
+    height: "540px",
+  }
+
+  function initViz() {
+    const onSuccess = function(parameters) {
+      parameters.map((parameter) => {
+        console.log("parameter.getName() : ", parameter.getName())
+        console.log(
+          "parameter.getCurrentValue() : ",
+          parameter.getCurrentValue()
+        )
+        console.log("parameter.getDataType() : ", parameter.getDataType())
+        console.log(
+          "parameter.getAllowableValuesType() : ",
+          parameter.getAllowableValuesType()
+        )
+
+        return null
+      })
+    }
+    const onFault = function() {}
+
+    const viz = new tableau.Viz(ref.current, url, {
+      ...options,
+      onFirstInteractive: () => {
+        const workbook = viz.getWorkbook()
+        const log = workbook.getParametersAsync().then(onSuccess, onFault)
+        console.log(log)
+      },
+    })
+
+    return null
+  }
 
   useEffect(() => {
-    const viz = new tableau.Viz(
-      ref.current,
-      "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DatePicker/Dashboard",
-      {
-        hideTabs: true,
-        hideToolbar: true,
-        width: "900px",
-        height: "540px",
-      }
-    )
-
-    return viz
+    initViz()
   })
 
   return <div ref={ref} />
