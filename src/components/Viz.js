9228e4d69e8821bfe97b8715bbff811637489109
robcrockPI <robert@propellerindustries.com>
2020-12-01 12:49:33 -0300

    Passed values into the paramter to update the viz

--- src/components/Viz.js
+++ src/components/Viz.js
@@ -3,76 +3,46 @@ import { useState, useRef, useEffect } from "react"
 const { tableau } = window
 
 export default function Viz() {
+  // State variables
   const [viz, setViz] = useState(null)
-  const [month, setMonth] = useState(11)
-  // Placeholder div
+  const [year, setYear] = useState(9)
+
   const ref = useRef(null)
   const url =
     "https://10ax.online.tableau.com/t/developmentonlydev595736/views/DatePicker/Dashboard"
-  const options = {
-    hideTabs: true,
-    hideToolbar: true,
-    width: "900px",
-    height: "540px",
-  }
-
-  const onSuccess = (parameters) => {
-    parameters.map((parameter) => {
-      console.log("parameter.getName() : ", parameter.getName())
-      console.log("parameter.getDataType() : ", parameter.getDataType())
-      console.log("parameter.getCurrentValue() : ", parameter.getCurrentValue())
-
-      return null
-    })
-  }
-
-  const onFault = () => {}
 
   const initViz = () => {
-    if (viz) {
-      viz.dispose()
-      setViz(
-        new tableau.Viz(ref.current, url, {
-          ...options,
-          onFirstInteractive: () => {
-            const workbook = viz.getWorkbook()
-            workbook.changeParameterValueAsync(
-              "End Date",
-              new Date(Date.UTC(2019, { month }, 1))
-            )
-          },
-        })
-      )
-      console.log("Month post dispose ", month)
-    } else {
-      setViz(
-        new tableau.Viz(ref.current, url, {
-          ...options, //,
-          // onFirstInteractive: () => {
-          //   const workbook = viz.getWorkbook()
-          //   workbook.getParametersAsync().then(onSuccess, onFault)
-          // },
-        })
-      )
+    let options = {
+      hideTabs: true,
+      hideToolbar: true,
+      width: "900px",
+      height: "540px",
     }
-    console.log("Month pre dispose ", month)
+    setViz(new tableau.Viz(ref.current, url, options))
   }
 
-  useEffect(initViz, [month])
+  // Initialize viz when the page loads
+  useEffect(initViz, [])
 
-  const updateMonth = (monthArg) => {
-    console.log(monthArg)
-    const workbook = viz.getWorkbook()
+  function changeYearParameterValue(value) {
+    setYear(value)
+    let workbook = viz.getWorkbook()
+
     workbook.changeParameterValueAsync(
       "End Date",
-      new Date(Date.UTC(2019, { monthArg }, 1))
+      new Date(Date.UTC(`201${value}`, 11, 1))
     )
-    setMonth(monthArg)
   }
 
   return (
     <>
-      <input onChange={(e) => updateMonth(e.target.value)} type="text" />
+      <lable for="end-date">End Date (enter a value between 6 and 9)</lable>
+      <input
+        type="text"
+        name="end-date"
+        onChange={(e) => changeYearParameterValue(e.target.value)}
+        value={year}
+      />
       <div ref={ref} />
     </>
   )
